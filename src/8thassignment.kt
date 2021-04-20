0e66bffdc957af6873688554de5973e39d6ad3c3
Okola44 <mercyimaliokola@gmail.com>
2021-04-20 21:57:32 +0300

    8thassignment

---
+++ src/8thassignment.kt
@@ -0,0 +1,36 @@
+fun main() {
+println (isEven(200))
+    names("mangoes")
+
+}
+fun isEven(num:Int):Boolean{
+   if (num%2==0){
+       return true
+   }
+    else{
+        return false
+   }
+}
+data class man(var name:String,var weight:Int,var price:Int,var category:String){
+    fun grouping(product:String){
+            var groceries= listOf<String>()
+        var hygene= listOf<String>()
+        when(product){
+
+        }
+    }
+}
+fun names(name:String):String{
+   var x=" "
+    for (v in name){
+        if (name.indexOf(v)%2==0){
+            print(v)
+        }
+    }
+return "${x}"
+       }
+fun things(names:Array<String>):ArrayList<String>{
+var namesArray=ArrayList<String>()
+    return namesArray
+
+}
